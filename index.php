969868137eabfc1c53967a5608d082412c7f8701
Daniel Matilla <daniel@esdis.es>
2017-03-21 21:56:32 +0100

    Added option to cut lines using / character

--- index.php
+++ index.php
@@ -4,13 +4,20 @@
     $image = $_GET['img'];
     $text = $_GET['text'];
 
-	$aux = explode(" ", $text);
-
-	$words = count($aux);
-	$chunk = ceil($words / 2);
-	$meme = array_chunk($aux, $chunk);
+    $aux = explode("/", $text);
+    if(count($aux) <= 1) {
+        $aux = explode(" ", $text);
+        $words = count($aux);
+        $chunk = ceil($words / 2);
+        $meme = array_chunk($aux, $chunk);
+        $textup = implode(" ", $meme[0]);
+        $textdown = implode(" ", $meme[1]);
+    } else {
+        $textup = trim($aux[0]);
+        $textdown = trim($aux[1]);
+    }
 
     $obj = new MemeGenerator($image);
-	$obj->setUpperText(implode(" ", $meme[0]));
-	$obj->setLowerText(implode(" ", $meme[1]));
+	$obj->setUpperText($textup);
+	$obj->setLowerText($textdown);
 	$obj->outputImg();
